3c9ffd32bb8b01271c45234654183a027b5d0e5e
gloriouspete <peter4@email.com>
2023-12-24 09:48:43 +0100

    too real

--- src/Main.js
+++ src/Main.js
@@ -4,8 +4,7 @@ import { Footr } from './header';
 import './Media.css';
 import { useState, useEffect, useCallback } from 'react';
 import { motion } from "framer-motion";
-import { FaLinkedin,FaGithub } from "react-icons/fa";
-import { DiAndroid, DiApple, DiCss3, DiGithub, DiJavascript, DiMongodb, DiMysql, DiNodejs, DiReact, DiTerminal, } from 'react-icons/di'
+import { DiAndroid, DiApple, DiNodejs, DiReact,} from 'react-icons/di'
 export const Main = () => {
   const contentArray = ["Mobile App Developer", "Backend Developer", "Bot Scripter"];
   const [contentIndex, setContentIndex] = useState(0);
@@ -33,80 +32,58 @@ export const Main = () => {
 
   return (
     <>
-      <div className="w-auto bg-black box-border">
-        <div className="sm:w-screen sm:h-4/5 bg-black" style={{
-           backgroundImage: "url('./globackground.jpg')",
-           backgroundSize: 'cover',
-           backgroundRepeat: 'no-repeat',}}>
-        <motion.h1
-         initial={{opacity:0,y:-200}}
-        animate={{ opacity: 1 , y: 0 }}
-        transition={{ duration: 2 }}
-         className="text-white text-center mb-2 sm:mb-5 text-2xl font-interbold after:text-mycolor mt-36  sm:mt-64 after:content-'Glo'">Hi,I'm Glo </motion.h1>
-        <div className="w-full flex justify-center">
-          <h1 className="text-sm text-white">{contentArray[contentIndex]}</h1>
+      <div className="w-auto bg-white box-border">
+        <div className="sm:w-screen sm:h-4/5 bg-white" style={{
+          backgroundImage: "url('./globackground.jpg')",
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+        }}>
+          <motion.h1
+            initial={{ opacity: 0, y: -200 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 2 }}
+            className="text-slate-900 text-center mb-2 sm:mb-5 text-2xl font-interbold after:text-mycolor mt-32  sm:mt-64 after:content-'Glo'">Hi,I'm Glo 👋</motion.h1>
+          <div className="w-full flex justify-center">
+            <h1 className="text-sm text-black text-center">{contentArray[contentIndex]}</h1>
+          </div>
+          <div className="">
+            <p className="font-interbold text-black text-2xl sm:text-2xl p-5 mt-3 sm:p-8 text-center sm:mb-8">
+              I empower your <span className="text-orange-600 font-interbold text-3xl" >Business</span> with state of the art digital solutions!
+            </p>
+            <p className="font-inter text-black text-base sm:text-2xl p-5 mt-3 sm:p-8 text-center sm:mb-8">
+              ...From captivating websites to seamless mobile applications,I have you fully covered!
+            </p>
+          </div>
+          <Homer />
         </div>
-        <div className="">
-          <p className="font-inter text-white text-lg sm:text-2xl p-1 mt-3 sm:p-8 text-center sm:mb-8">
-            I empower your business with state of the art digital solutions!,From captivating websites to seamless mobile applications,I have you fully covered!
-          </p>
-        </div>
-        <Homer />
-        </div>
-        <h3 className="text-center text-slate-400 text-2xl font-bold mt-16 mb-3">
-          Skills
+        <h3 className="text-center text-slate-700 text-lg font-interbold mt-16 mb-3">
+          Skilled with
         </h3>
 
-        <h1 className="text-center font-inter mb-3 px-4 text-white">
-          These are the stacks i currently work with.
-        </h1>
-        <div className="toolstack">
-          <div className="flex flex-col h-auto p-2 items-center">
-            <DiJavascript size={30} color="#0000cf" />
-          <p className="text-white">Javascript</p>
-          </div>
-          <div className="flex flex-col h-auto p-2 items-center">
-            <DiMysql size={30} color="#0000fff0"/>
-             <p className="text-white">MySql</p>
-          </div>
-          <div className="flex flex-col h-auto p-2 items-center">
-            <DiReact size={30} color="#0000cf" />
-          <p className="text-white">React Js</p>
-          </div>
-          <div className="flex flex-col h-auto p-2 items-center">
-            <DiNodejs size={30} color="#00cf00" />
-          <p className="text-white">Node Js</p>
-          </div>
-          <div className="flex flex-col h-auto p-2 items-center">
-            <DiCss3 size={30} color="#00a0ff" />
-          <p className="text-white">Css3</p>
-          </div>
-          <div className="flex flex-col h-auto p-2 items-center">
-            <DiTerminal size={30} color="#ff0000" />
-          <p className="text-white">Terminal</p>
-          </div>
-          <div className="flex flex-col h-auto p-2 items-center">
-            <FaGithub size={30} color="#ffffff" />
-          <p className="text-white">Git & Github</p>
-          </div>
-          <div className="flex flex-col h-auto p-2 items-center">
-            <DiMongodb size={30} color="#00ffcf" />
-          <p className="text-white">MongoDb</p>
-          </div>
-          <div className="flex flex-col h-auto p-2 items-center">
-            <DiAndroid size={30} color="#00bf00" />
-          <p className="text-white">Android</p>
-          </div>
-          <div className="flex flex-col h-auto p-2 items-center">
-            <DiApple size={30} color="#0000cf" />
-          <p className="text-white">iOS</p>
-          </div>
+        <div className="w-full flex flex-col sm:flex-row sm:justify-center h-auto items-center">
+
+          <DiNodejs size={95} className="fill-green-600 " />
+
+
+          <DiReact size={70} className="fill-black " />
+
+          <DiAndroid size={70} className="fill-green-400 " />
+
+          <DiApple size={70} className="fill-black " />
+
+        </div>
+        <div className="w-full h-auto flex flex-col mt-10 items-center">
+          <h1 className="text-center text-slate-900 font-interbold text-2xl ">Go from 0 to App in days not 3 Months</h1>
+
+          <p className="text-center font-inter text-lg px-5 mt-5">Unlike Typical Devs, I already possess feature-packed <b>boilerplates</b> that ships with auth, payments, in-app purchases, data-handling and real-time alerts plus notifications. 
+          Launch your mobile app or websites faster and quicker with <b>Me</b>.</p>
+
         </div>
         <Project />
         <div className="w-full h-auto shadow-md shadow-slate-400 p-5 mt-4 ">
           <br />
 
-          <p className="text-slate-400 text-lg m-3 ">
+          <p className="text-slate-500 text-lg m-3 ">
             CONTACT
           </p>
           <div className="concover">
@@ -114,12 +91,7 @@ export const Main = () => {
             <div className="upcover">
             </div>
             <div className="downcover">
-              <h1 className="text-white font-inter font-semibold text-center">
-                Gloriouspete
-              </h1>
-              <p className="text-white font-semibold text-center p-2">
-                FULL-STACK DEVELOPER
-              </p>
+             
               <Homer />
             </div>
           </div>
--- src/Projection.js
+++ src/Projection.js
@@ -7,6 +7,8 @@ import heydata from './heydata.png'
 import mysearch from './mysearch.png'
 import redditcrawler from './redditcrawler.png'
 import opine from './opine.png'
+import screen2 from './screen2.png'
+import screen4 from './screen4.png'
 import { Github, Demo } from './misc'
 import { FaGithub } from 'react-icons/fa';
 
@@ -15,140 +17,68 @@ export const Project = () => {
     return (
         <>
             <div className="flex flex-col w-screen h-auto items-center mt-6 flex-wrap ">
-                <p className="text-center text-2xl font-semibold text-slate-500" >My Projects</p>
-                <h3 className='p-4 text-center text-slate-200'>Here are some of my Works</h3>
+                <p className="text-center text-2xl font-semibold text-slate-500 my-5" >My Projects</p>
                 <div className="flex flex-row justify-around w-screen h-auto items-center flex-wrap ">
-                    <div className='projectbox1 w-11/12 sm:w-5/12 md:4/12 h-auto shadow-sm shadow-slate-400 pb-6 mb-10 rounded-md'>
+
+
+                    <div className='projectbox1 w-11/12 sm:w-5/12 md:4/12 h-auto shadow-md inset-0 shadow-green-400 py-6 mb-10 rounded-md'>
                         <div className='w-full h-64'>
+                        <a href="https://heydata.com.ng">
                             <img src={heydata} alt="Description of" className='w-full h-full rounded-md' />
+                            </a>
                         </div>
-                        <div className=''>
-                            <div className='flex w-full h-auto p-2 justify-between items-center'>
-                                <h2 className='font-semibold text-white'>HeyData</h2>
-                                <div className='redbox'>
-                                    <a href="https://heydata.com.ng"><Demo /></a>
-                                    <Github onClick={() => alert('Github File for this project is hidden')} className="fill-white" />
-                                </div>
-                            </div>
-                            <p className='font-inter text-sm px-3 pb-4 text-center text-slate-300'>
-                                HeyData Mobile Service is a Vtu website made for buying Mobile Data and airtime and other utility services.
-
-                            </p>
-                            <div className='flex flex-wrap justify-around'>
-                                <p className='w-auto h-auto p-2 text-white font-intermedium rounded-md bg-blue-600'>Next js</p>
-                                <p className='w-auto h-auto p-2 text-white font-intermedium rounded-md bg-blue-600'>Express Js</p>
-                                <p className='w-auto h-auto p-2 text-white font-intermedium rounded-md bg-blue-600'>MysQl</p>
-                                <p className='w-auto h-auto p-2 text-white font-intermedium rounded-md bg-blue-600'>Api</p>
-                            </div>
-                        </div>
+                       
 
                     </div>
-                    <div className='projectbox2 w-11/12 sm:w-5/12 h-auto shadow-sm shadow-slate-400 pb-6 mb-10 rounded-md'>
+                    <div className='projectbox2 w-11/12 sm:w-5/12 h-auto shadow-sm shadow-slate-400 py-6 mb-10 rounded-md'>
                         <div className='w-full h-64'>
+                        <a href="https://redditcrawler.vercel.app">
                             <img src={redditcrawler} alt="Description of" className='w-full h-full rounded-md' />
+                            </a>
                         </div>
-                        <div className=''>
-                            <div className='flex w-full h-auto p-2 justify-between items-center'>
-                                <h2 className='font-semibold text-white'>Reddit Crawler</h2>
-                                <div className='redbox'>
-                                    <a href="https://redditcrawler.vercel.app"><Demo /></a>
-                                    <Github onClick={() => alert('Github File for this project is hidden')} />
-                                </div>
-                            </div>
-                            <p className='font-inter text-sm px-3 pb-4 text-slate-300 text-center'>
-                                Reddit Crawler is a website I made to simplify the stress of looking for work related posts on reddit, it filters the excessive ads and brings only related posts to the one you queried.
-
-                            </p>
-                            <div className='flex flex-wrap justify-around'>
-                                <p className='w-auto h-auto p-2 text-white font-intermedium rounded-md bg-blue-600'>Node js</p>
-                                <p className='w-auto h-auto p-2 text-white font-intermedium rounded-md bg-blue-600'>Express</p>
-                                <p className='w-auto h-auto p-2 text-white font-intermedium rounded-md bg-blue-600'>Api</p>
-                                <p className='w-auto h-auto p-2 text-white font-intermedium rounded-md bg-blue-600'>Static</p>
-                            </div>
-                        </div>
-
                     </div>
+
                     <div className='projectbox3 w-11/12 sm:w-5/12 h-auto shadow-sm shadow-slate-400 pb-6 mb-10 rounded-md'>
                         <div className='w-full h-64'>
+                        <a href="https://opine.vercel.app">
                             <img src={mysearch} alt="Description of" className='w-full h-full rounded-md' />
-                        </div>
-                        <div className=''>
-                            <div className='flex w-full h-auto p-2 justify-between items-center'>
-                                <h2 className='font-semibold text-white'>Opine</h2>
-                                <div className='redbox'>
-                                    <a href="https://opine.vercel.app"><Demo /></a>
-                                    <Github onClick={() => alert('Github File for this project is hidden')} />
-                                </div>
-                            </div>
-                            <p className='font-inter text-slate-300 text-sm px-3 pb-4 text-center'>
-                                Opine is a website created to tackle the issues of low engagements in parties and hangouts, It works by accepting questions,opinions and suggestions from the audience and then the hosts can view them on the website and answer accordingly.
-                            </p>
-                            <div className='flex flex-wrap justify-around'>
-                                <p className='w-auto h-auto p-2 text-white font-intermedium rounded-md bg-blue-600'>React js</p>
-                                <p className='w-auto h-auto p-2 text-white font-intermedium rounded-md bg-blue-600'>Express js</p>
-                                <p className='w-auto h-auto p-2 text-white font-intermedium rounded-md bg-blue-600'>MysQl</p>
-                                <p className='w-auto h-auto p-2 text-white font-intermedium rounded-md bg-blue-600'>Node js</p>
-                            </div>
+                            </a>
                         </div>
 
                     </div>
                     <div className='projectbox4 w-11/12 sm:w-5/12 h-auto shadow-sm shadow-slate-400 pb-6 mb-10 rounded-md'>
                         <div className='w-full h-64'>
+                        <a href="https://translapp.vercel.app">
                             <img src={translapp} alt="Description of" className='w-full h-full rounded-md' />
+                            </a>
                         </div>
-                        <div className=''>
-                            <div className='flex w-full h-auto p-2 justify-between items-center'>
-                                <h2 className='font-semibold text-white'>Translapp</h2>
-                                <div className='redbox'>
-                                    <a href="https://translapp.vercel.app"><Demo /></a>
-                                    <FaGithub size={10} onClick={() => alert('Github File for this project is hidden')} />
-                                </div>
-                            </div>
-                            <p className='font-inter text-sm px-3 pb-4 text-center text-slate-300'>
-                                Translapp as it's name implies is a language translator web application, it translates over 40 languages with just a click in the most seamless way possible.
+                       
 
-                            </p>
-                            <div className='flex flex-wrap justify-around'>
-                                <p className='w-auto h-auto p-2 text-white font-intermedium rounded-md bg-blue-600'>React js</p>
-                                <p className='w-auto h-auto p-2 text-white font-intermedium rounded-md bg-blue-600'>Express</p>
-                                <p className='w-auto h-auto p-2 text-white font-intermedium rounded-md bg-blue-600'>REST Api</p>
-                            </div>
+                    </div>
+
+                    <div className='projectbox5 w-11/12 sm:w-5/12 h-auto shadow-sm shadow-slate-400 pb-6 mb-10 rounded-md'>
+                        <div className='w-full h-64'>
+                        <a href="https://translapp.vercel.app">
+                            <img src={opine} alt="Description of" className='w-full h-full rounded-md' />
+                            </a>
                         </div>
 
                     </div>
-                    <div className='projectbox4 w-11/12 sm:w-5/12 h-auto shadow-sm shadow-slate-400 pb-6 mb-10 rounded-md'>
-                        <div className='w-full h-64'>
-                            <img src={opine} alt="Description of" className='w-full h-full rounded-md' />
-                        </div>
-                        <div className=''>
-                            <div className='flex w-full h-auto p-2 justify-between items-center'>
-                                <h2 className='font-semibold text-white'>MySearch</h2>
-                                <div className='redbox'>
-                                    <a href="https://translapp.vercel.app"><Demo /></a>
-                                    <FaGithub size={10} onClick={() => alert('Github File for this project is hidden')} />
-                                </div>
-                            </div>
-                            <p className='font-inter text-sm px-3 pb-4 text-center text-slate-300'>
-                                Mysearch is an alternative to google, it's a search engine that works almost the way the legendary google works.
 
-                            </p>
-                            <div className='flex flex-wrap justify-around'>
-                                <p className='w-auto h-auto p-2 text-white font-intermedium rounded-md bg-blue-600'>Next Js</p>
-                                <p className='w-auto h-auto p-2 text-white font-intermedium rounded-md bg-blue-600'>Express</p>
-                                <p className='w-auto h-auto p-2 text-white font-intermedium rounded-md bg-blue-600'>REST Api</p>
-                            </div>
+                    <div className='projectbox6 w-11/12 sm:w-5/12 h-auto shadow-sm shadow-slate-400 pb-6 mb-10 rounded-md'>
+                        <div className='w-full h-auto flex justify-around'>
+                      
+                            <img src={screen2} alt="Description of" className='w-24 h-full rounded-md' />
+                            <img src={screen4} alt="Description of" className='w-24 h-full rounded-md' />
+                    
                         </div>
 
                     </div>
 
                     <div className='w-full h-auto flex flex-col justify-center items-center mt-8'>
-                        <p className='text-center text-lg font-interbold text-white'>Impressed?</p>
-                        <p className='text-md text-slate-400 font-inter'>Well, you should!</p>
-                        <p className=' font-intermedium px-2 mt-3 text-center text-white'>I make mobile apps too and a few are deployed to playstore</p>
+                        <p className='text-center text-lg font-interbold text-slate-900'>Skip the exhaustive steps! 🚀</p>
+                        <p className='text-md text-slate-700 font-inter text-center'>I work independently yet in alliance with your predefined goals</p>
                     </div>
-                </div>
-                <div className='w-full flex justify-center items-center mt-4'>
-                    <Link to='https://play.google.com/store/apps/details?id=com.gloriouspete.nativea'><button className="w-auto h-auto px-3 py-2 shadow-sm shadow-slate-400 rounded-md text-md font-semibold hover:bg-black hover:text-white text-white bg-blue-400">Show me</button></Link>
                 </div>
             </div>
         </>
